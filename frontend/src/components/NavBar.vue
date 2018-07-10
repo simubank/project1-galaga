dd1a973b755266c0f0790efe46be8819732eb530
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2018-07-10 11:21:03 -0400

    add files

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">LOL</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                  <router-link class="nav-link" to="/">Home</router-link>
+              </li>
+              <li class="nav-item">
+                  <router-link class="nav-link" to="/about">About</router-link>
+              </li>
+          </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
