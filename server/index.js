1411789ec3b426247216f3bfa01289b7191e617d
Yuxuan Zhang <edwinyuxuan.zhang@mail.utoronto.ca>
2018-07-17 09:49:03 -0400

    add customer transaction end-point

--- server/index.js
+++ server/index.js
@@ -15,7 +15,21 @@ app.get('/fundInfo/:id',function(req, res){
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.parse(dataString));
     });
+});
 
+app.get('/customer/trasaction/:customerId',function(req, res){
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python3',["../engine/getCustomerTransaction.py", req.params.customerId]);
+    var dataString = '';
+    pythonProcess.stdout.on('data', (data) => {
+          dataString += data.toString();
+    });
+    pythonProcess.stdout.on('end', function(){
+        dataString = dataString.replace(/[\n\r]/g, '');
+        dataString = dataString.replace(/'/g,'"');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.parse(dataString));
+    });
 });
 
 
