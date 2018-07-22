412b16e772d05b69a226b6216711f86c29d5800a
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2018-07-22 17:08:14 -0400

    add data to pie chart and auto load again on data change

--- app/src/views/dashboard/admin/components/PieChart.vue
+++ app/src/views/dashboard/admin/components/PieChart.vue
@@ -6,7 +6,6 @@
 import echarts from 'echarts'
 require('echarts/theme/macarons') // echarts theme
 import { debounce } from '@/utils'
-import { fetchList } from '@/api/transaction'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -27,12 +26,13 @@ export default {
   data() {
     return {
       chart: null,
-      list: null,
-      category: null
+      list: [],
+      category: []
     }
   },
   mounted() {
     this.initChart()
+    this.updateChart()
     this.__resizeHanlder = debounce(() => {
       if (this.chart) {
         this.chart.resize()
@@ -50,49 +50,66 @@ export default {
   },
   computed: {
     ...mapGetters([
-      'netSaving'
+      'netSaving',
+      'listLoading',
+      'netSpending',
+      'netIncome',
+      'netSpendingList'
     ])
+  },
+  watch: {
+    netSaving: function(val) {
+      this.updateChart()
+    }
   },
   methods: {
     initChart() {
       this.chart = echarts.init(this.$el, 'macarons')
-      fetchList().then(response => {
-        this.list = response.data.items.map(function(item) {
+    },
+    setChart() {
+      this.chart.setOption({
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          left: 'center',
+          bottom: '10',
+          data: this.category
+        },
+        calculable: true,
+        series: [{
+          name: 'spending distribution',
+          type: 'pie',
+          roseType: 'radius',
+          radius: [15, 95],
+          center: ['50%', '38%'],
+          data: this.list,
+          animationEasing: 'cubicInOut',
+          animationDuration: 2600
+        }]
+      })
+    },
+    updateChart() {
+      if (!this.listLoading) {
+        this.list = this.netSpendingList.map(function(item) {
           return {
             value: item.amount,
             name: item.category
           }
         })
+        this.category = this.netSpendingList.map(function(item) {
+          return item.category
+        })
         this.list.push({
           value: this.netSaving,
           name: 'Saving'
         })
-        this.category = response.data.category
         this.category.push('Saving')
-        this.chart.setOption({
-          tooltip: {
-            trigger: 'item',
-            formatter: '{a} <br/>{b} : {c} ({d}%)'
-          },
-          legend: {
-            left: 'center',
-            bottom: '10',
-            data: this.category
-          },
-          calculable: true,
-          series: [{
-            name: 'spending distribution',
-            type: 'pie',
-            roseType: 'radius',
-            radius: [15, 95],
-            center: ['50%', '38%'],
-            data: this.list,
-            animationEasing: 'cubicInOut',
-            animationDuration: 2600
-          }]
-        })
-      })
+        this.setChart()
+      }
     }
+
   }
 }
 </script>
