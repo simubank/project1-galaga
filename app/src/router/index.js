d6e08a72a984a90643fadfa33cd7e49504aa811e
Yuxuan Zhang <edwinyuxuan.zhang@mail.utoronto.ca>
2018-07-23 00:22:18 -0400

    delete error icons on sidebar

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -27,8 +27,8 @@ import Layout from '@/views/layout/Layout'
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
   { path: '/authredirect', component: () => import('@/views/login/authredirect'), hidden: true },
-  { path: '/404', component: () => import('@/views/errorPage/404'), hidden: true },
-  { path: '/401', component: () => import('@/views/errorPage/401'), hidden: true },
+  // { path: '/404', component: () => import('@/views/errorPage/404'), hidden: true },
+  // { path: '/401', component: () => import('@/views/errorPage/401'), hidden: true },
   {
     path: '',
     component: Layout,
@@ -59,20 +59,4 @@ export default new Router({
   routes: constantRouterMap
 })
 
-export const asyncRouterMap = [
-  {
-    path: '/error',
-    component: Layout,
-    redirect: 'noredirect',
-    name: 'errorPages',
-    meta: {
-      title: 'errorPages',
-      icon: '404'
-    },
-    children: [
-      { path: '401', component: () => import('@/views/errorPage/401'), name: 'page401', meta: { title: 'page401', noCache: true }},
-      { path: '404', component: () => import('@/views/errorPage/404'), name: 'page404', meta: { title: 'page404', noCache: true }}
-    ]
-  },
-  { path: '*', redirect: '/404', hidden: true }
-]
+export const asyncRouterMap = []
