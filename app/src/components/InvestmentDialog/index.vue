018333381fcc9a10b5316ee024de2653d0fe0def
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2018-07-24 11:15:24 -0400

    add btn link to webbroker

--- app/src/components/InvestmentDialog/index.vue
+++ app/src/components/InvestmentDialog/index.vue
@@ -22,7 +22,9 @@
         <p>If you made the investment <b>1 Year ago</b> with TD, you would have made: <b>${{investmentGain.Yr1}}</b> and your saving will be: <b>${{Math.round((investmentGain.Yr1 + netSaving) * 100) / 100}}</b>!</p>
         <p>If you made the investment <b>5 Year ago</b> with TD, you would have made: <b>${{investmentGain.Yr5}}</b> and your saving will be: <b>${{Math.round((investmentGain.Yr5 + netSaving) * 100) / 100}}</b>!</p>
         <p>It's never TOO LATE to start investment, talk with TD to find the best investment options for your!</p>
-        <el-button v-on:click.prevent="goTDInvest" type="success">Request a Call Back</el-button>
+        <el-button v-on:click.prevent="goTDInvest('call')" type="success">Request a Call Back</el-button>
+        <p>Or you can start invest right now!</p>
+        <el-button v-on:click.prevent="goTDInvest('account')" type="success">Invest Now</el-button>
         <div class="disclaimer"><p>Disclaimer: This Projected Saving displays what your portfolio income might look like based on previous historical performance.
           This is for estimation purpose ONLY, and TD <b> does NOT </b> garuantee you will make the same amount of money. </p>
         </div>
@@ -56,8 +58,8 @@ export default {
       this.$store.dispatch('closeInvestmentDialog')
       done()
     },
-    goTDInvest() {
-      const url = 'https://www.forms.td.com/app/di-request-call/#/di-request-call/di-request-call-form'
+    goTDInvest(str) {
+      var url = str === 'call' ? 'https://www.forms.td.com/app/di-request-call/#/di-request-call/di-request-call-form' : 'https://wb.authentication.td.com/uap-ui/index.html?consumer=webbroker&locale=en_CA#/login/webbroker-getting-started'
       window.open(url, '_blank')
     }
   }
