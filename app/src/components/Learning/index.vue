eab858158431ffb6001261af0cfce638f3290670
itimie <itimieloesther@gmail.com>
2018-07-23 23:43:13 -0400

    learning

--- app/src/components/Learning/index.vue
+++ app/src/components/Learning/index.vue
@@ -1,16 +1,28 @@
 <template>
-    <el-card class = "learning">
-      <p> {{message}}</p>
-    </el-card>
+
+  <!--Want to know more about mutual funds? click here -->
+  <div>
+  <h2> Want to book a meeting with a financial advisor?? </h2>
+  <el-button type="success" style="width:200px; margin:5px 30px; float:left" @click.native.prevent="simulate">Book an appointment</el-button>
+  </div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'learning',
+  name: 'Learning',
   data() {
     return {
-      message: test
+      message: 'test'
+    }
+  },
+
+  methods: {
+    simulate() {
+      console.log('try simulate')
     }
   }
+
 }
 </script>
--- app/src/views/learn/index.vue
+++ app/src/views/learn/index.vue
@@ -1,13 +1,25 @@
 <template>
-    <el-row :gutter="20">
-        <el-col :xs="{span: 24}" :sm="{span: 24}" :lg="{span: 24}" v-for="(fundID, index) in fundList" :key="index">
-          <learning></learning>
-        </el-col>
-        <el-button type="success" style="width:200px; margin:5px 30px; float:right" @click.native.prevent="simulate">Simulate</el-button>
+
+  <!--Want to know more about mutual funds? click here -->
+  <div>
+    <el-row :gutter="32">
+        <el-col :xs="24" :sm="24" :lg="24">
+        <p style="width:200px; margin:10px 100px; float:left"> Mutual funds 101
+          <el-button v-bind:href='https://www.td.com/ca/en/personal-banking/how-to/saving-investing/invest-gics-term-deposits-mutual-funds/?tdtab=2' type="success" style="width:200px; margin:5px 30px; float:right">Learn More</el-button> </p>
+
+      </el-col>
     </el-row>
+  </div>
+
+
 </template>
 
 <script>
+
+// add information about about how to book a phone call
+// add information page to link to be able to get mutual funds
+// add information to help ppl learn more about mutual funds
+
 import learning from '@/components/Learning'
 import { mapGetters } from 'vuex'
 
